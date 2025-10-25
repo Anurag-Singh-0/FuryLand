0280b2a9b062fc8f92ad665aa51d6e26c29e3d3b
Anurag Singh <147307671+Anurag-Singh-0@users.noreply.github.com>
2025-10-25 23:26:34 +0530

    successfully write the product adding code

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -5,11 +5,22 @@ import {
   listProducts,
   removeProduct,
   singleProduct,
-} from "../controllers/productController";
+} from "../controllers/productController.js";
+
+import upload from "../middleware/multer.js";
 
 const productRoute = express.Router();
 
-productRoute.post("/add", addProduct);
+productRoute.post(
+  "/add",
+  upload.fields([
+    { name: "image1", maxCount: 1 },
+    { name: "image2", maxCount: 1 },
+    { name: "image3", maxCount: 1 },
+    { name: "image4", maxCount: 1 },
+  ]),
+  addProduct
+);
 productRoute.post("/remove", removeProduct);
 productRoute.post("/single", singleProduct);
 productRoute.get("/list", listProducts);
