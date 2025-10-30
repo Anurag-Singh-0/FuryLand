2fed41ba2606bb9ab169f8c12c129603350e9e7f
Anurag Singh <147307671+Anurag-Singh-0@users.noreply.github.com>
2025-10-31 00:14:01 +0530

    changing the code

--- admin/admin-panel/src/components/Navbar.jsx
+++ admin/admin-panel/src/components/Navbar.jsx
@@ -1,10 +1,43 @@
-import MenuOpenIcon from "@mui/icons-material/MenuOpen";
+import { NavLink } from "react-router-dom";
+
+import {
+  SpaceDashboard as DashboardIcon,
+  Assignment as OrdersIcon,
+  Category as ProductsIcon,
+  Inventory2 as InventoryIcon,
+} from "@mui/icons-material";
 
 function Navbar() {
+  const navItems = [
+    { name: "Dashboard", path: "/dashboard", icon: DashboardIcon },
+    { name: "Add Item", path: "/add", icon: ProductsIcon },
+    { name: "List Items", path: "/list", icon: InventoryIcon },
+    { name: "Orders", path: "/orders", icon: OrdersIcon },
+  ];
+
   return (
     <div>
       {/* Left section */}
-      <div></div>
+      <div>
+        <img
+          src="../../public/Admin-logo.png"
+          alt="admin-logo"
+          className="w-30 sm:w-50"
+        />
+
+        <nav>
+          {navItems.map(({ name, path, icon: Icon }) => (
+            <NavLink key={name} to={path}>
+              {
+                <>
+                  <Icon />
+                  <span>{name}</span>
+                </>
+              }
+            </NavLink>
+          ))}
+        </nav>
+      </div>
 
       {/* Right section */}
       <div></div>
