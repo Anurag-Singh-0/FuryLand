0c2f63cc3cea9619583c5b07eccb237ac6194247
Anurag Singh <147307671+Anurag-Singh-0@users.noreply.github.com>
2025-10-31 00:52:05 +0530

    changing style

--- admin/admin-panel/src/components/Navbar.jsx
+++ admin/admin-panel/src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import { NavLink } from "react-router-dom";
+import MenuOpenIcon from "@mui/icons-material/MenuOpen";
 
 import {
   SpaceDashboard as DashboardIcon,
@@ -18,25 +19,41 @@ function Navbar() {
   return (
     <div>
       {/* Left section */}
-      <div>
+      <div className="bg-gray-100 flex flex-col py-10 px-5 w-[18%] gap-10">
         <img
           src="../../public/Admin-logo.png"
           alt="admin-logo"
           className="w-30 sm:w-50"
         />
 
-        <nav>
+        <nav className="flex flex-col gap-5">
+          <span className="text-gray-500 text-sm">Menu</span>
           {navItems.map(({ name, path, icon: Icon }) => (
-            <NavLink key={name} to={path}>
-              {
+            <NavLink
+              key={name}
+              to={path}
+              className={({ isActive }) =>
+                `flex items-center gap-3 px-4 py-2 rounded-full font-medium transition-all duration-200 ${
+                  isActive
+                    ? "bg-[#003DF6] text-white shadow-md"
+                    : "text-gray-600 hover:bg-gray-100 hover:text-gray-900"
+                }`
+              }
+            >
+              {({ isActive }) => (
                 <>
-                  <Icon />
+                  <Icon
+                    className={` ${isActive ? "text-white" : "text-gray-500"}`}
+                  />
                   <span>{name}</span>
                 </>
-              }
+              )}
             </NavLink>
           ))}
         </nav>
+
+        <button>Logout</button>
+        <MenuOpenIcon />
       </div>
 
       {/* Right section */}
