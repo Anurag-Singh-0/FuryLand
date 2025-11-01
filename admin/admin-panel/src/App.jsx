7289fb5931b140c6242f032d73f12e82796f1443
Anurag Singh <147307671+Anurag-Singh-0@users.noreply.github.com>
2025-11-01 21:28:32 +0530

    building a route

--- admin/admin-panel/src/App.jsx
+++ admin/admin-panel/src/App.jsx
@@ -1,12 +1,20 @@
 import Navbar from "./components/Navbar.jsx";
-
-
+import { Routes, Route } from "react-router-dom";
+import { AddProduct, Dashboard, ListProduct, Orders } from "./index.js";
 
 function App() {
   return (
     <div className="flex min-h-screen">
       <Navbar />
 
+      <div className="flex-1 bg-gray-50 p-6 md:ml-0 mt-16 md:mt-0">
+        <Routes>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/add" element={<AddProduct />} />
+          <Route path="/list" element={<ListProduct />} />
+          <Route path="/orders" element={<Orders />} />
+        </Routes>
+      </div>
     </div>
   );
 }
---
+++ admin/admin-panel/src/index.js
@@ -0,0 +1,6 @@
+import AddProduct from "./pages/AddProduct.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
+import ListProduct from "./pages/ListProducts.jsx";
+import Orders from "./pages/Orders.jsx";
+
+export { AddProduct, Dashboard, ListProduct, Orders };
