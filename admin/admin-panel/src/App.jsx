1035e7b7c9366682720c1e3229825cbc55bd2637
Anurag Singh <147307671+Anurag-Singh-0@users.noreply.github.com>
2025-11-04 23:41:47 +0530

    writing code for login page

--- admin/admin-panel/src/App.jsx
+++ admin/admin-panel/src/App.jsx
@@ -1,20 +1,30 @@
 import Navbar from "./components/Navbar.jsx";
 import { Routes, Route } from "react-router-dom";
 import { AddProduct, Dashboard, ListProduct, Orders } from "./index.js";
+import { useState } from "react";
+import Login from "./components/Login.jsx";
 
 function App() {
+  const [token, setToken] = useState("");
+
   return (
     <div className="flex min-h-screen">
-      <Navbar />
+      {token === "" ? (
+        <Login />
+      ) : (
+        <>
+          <Navbar />
 
-      <div className="flex-1 bg-gray-50 p-6 md:ml-0 mt-16 md:mt-0">
-        <Routes>
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/add" element={<AddProduct />} /> 
-          <Route path="/list" element={<ListProduct />} />
-          <Route path="/orders" element={<Orders />} />
-        </Routes>
-      </div>
+          <div className="flex-1 bg-gray-50 p-6 md:ml-0 mt-16 md:mt-0">
+            <Routes>
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/add" element={<AddProduct />} />
+              <Route path="/list" element={<ListProduct />} />
+              <Route path="/orders" element={<Orders />} />
+            </Routes>
+          </div>
+        </>
+      )}
     </div>
   );
 }
