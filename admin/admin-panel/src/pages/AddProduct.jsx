ddb46486296949e5d3efd670205d69b9a954db14
Anurag Singh <147307671+Anurag-Singh-0@users.noreply.github.com>
2025-11-12 00:52:32 +0530

    Creating structure of Add Product section

--- admin/admin-panel/src/pages/AddProduct.jsx
+++ admin/admin-panel/src/pages/AddProduct.jsx
@@ -4,27 +4,27 @@ function AddProduct() {
   return (
     <>
       <form>
-        <div>
-          <p>Upload Image</p>
+        <div className="">
+          <p className="mb-3">Upload Image</p>
 
-          <div className="">
+          <div className="flex gap-5">
             <label htmlFor="image1" className="">
-              <img src="../../public/upload.png" className="w-25"/>
+              <img src="../../public/upload.png" className="w-20 cursor-pointer"/>
               <input type="file" id="image1" hidden/>
             </label>
 
             <label htmlFor="image2" className="">
-              <img src="../../public/upload.png" className="w-25"/>
+              <img src="../../public/upload.png" className="w-20 cursor-pointer"/>
               <input type="file" id="image2" hidden/>
             </label>
 
             <label htmlFor="image3" className="">
-              <img src="../../public/upload.png" className="w-25"/>
+              <img src="../../public/upload.png" className="w-20 cursor-pointer"/>
               <input type="file" id="image3" hidden/>
             </label>
 
             <label htmlFor="image4" className="">
-              <img src="../../public/upload.png" className="w-25"/>
+              <img src="../../public/upload.png" className="w-20 cursor-pointer"/>
               <input type="file" id="image4" hidden/>
             </label>
 
