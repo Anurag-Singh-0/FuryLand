9ca59c9b3da6b4b3da73e2c79b8cc5b0f3969000
Anurag Singh <147307671+Anurag-Singh-0@users.noreply.github.com>
2025-11-12 00:50:04 +0530

    Creating Add Product section

--- admin/admin-panel/src/pages/AddProduct.jsx
+++ admin/admin-panel/src/pages/AddProduct.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
 
 function AddProduct() {
   return (
-    <div>AddProduct</div>
-  )
+    <>
+      <form>
+        <div>
+          <p>Upload Image</p>
+
+          <div className="">
+            <label htmlFor="image1" className="">
+              <img src="../../public/upload.png" className="w-25"/>
+              <input type="file" id="image1" hidden/>
+            </label>
+
+            <label htmlFor="image2" className="">
+              <img src="../../public/upload.png" className="w-25"/>
+              <input type="file" id="image2" hidden/>
+            </label>
+
+            <label htmlFor="image3" className="">
+              <img src="../../public/upload.png" className="w-25"/>
+              <input type="file" id="image3" hidden/>
+            </label>
+
+            <label htmlFor="image4" className="">
+              <img src="../../public/upload.png" className="w-25"/>
+              <input type="file" id="image4" hidden/>
+            </label>
+
+          </div>
+        </div>
+      </form>
+    </>
+  );
 }
 
-export default AddProduct
+export default AddProduct;
